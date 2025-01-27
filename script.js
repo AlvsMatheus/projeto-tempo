b05097fd2c32d5d3868346fb886924ad8765ff26
Matheus Alves <matheussilvaemac@gmail.com>
2025-01-27 12:49:33 -0300

    terminei o javascript

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let img = window.document.getElementById('#container')
-let horario = window.document.getElementById('#child')
-let data = new Date()
-let tempo = data.getHours()
-horario.innerHTML = console.log(`${horario} Horas`)
+function load() {
+    let msg = window.document.getElementById('horario')
+    let container = document.getElementById('container')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `${hora} Horas`
+
+    if (hora >= 0 && hora < 12) {
+        // Morning
+        container.style.backgroundImage = "url('manha.jpg')"
+        horario.style.color = "#FFF9C4"
+    } else if (hora >= 12 && hora < 18) {
+        // Afternoon
+        container.style.backgroundImage = "url('afternoon.jpg')"
+        horario.style.color = "#90CAF9"
+    } else {
+        // Night
+        container.style.backgroundImage = "url('night.jpg')"
+        horario.style.color = "#4682B4"
+    }
+}
+
+
